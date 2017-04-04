ce48b7e8f799f9520435476f5882c6d268fac6c9
Keeno <joseph.magtalas@gmail.com>
2017-04-04 19:04:29 +0800

    udpate files

---
+++ app/src/main/java/com/joseph/module/codegeneratorandscanner/AnyOrientationCaptureActivity.java
@@ -0,0 +1,14 @@
+package com.joseph.module.codegeneratorandscanner;
+
+/**
+ * Created by keeno on 04/04/2017.
+ */
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * This Activity is exactly the same as CaptureActivity, but has a different orientation
+ * setting in AndroidManifest.xml.
+ */
+public class AnyOrientationCaptureActivity extends CaptureActivity {
+
+}
